8d586113ca1a7096b6be83178392d6a1eb4cbf66
Arif <marifullaah@gmail.com>
2022-08-09 21:32:44 +0600

    print all book for page

---
+++ Book.js
@@ -0,0 +1,10 @@
+function paperRequirements(first, second, third) {
+    const firstBookPage = 100;
+    const secondBookPage = 200;
+    const thirdBookPge = 300;
+    const firstBookAllPage = firstBookPage * first;
+    const secondBookAllPage = secondBookPage * second;
+    const thirdBookAllPage = thirdBookPge * third;
+    return totalPage = firstBookAllPage + secondBookAllPage + thirdBookAllPage;
+}
+console.log("you need to page for all book " + paperRequirements(2, 3, 4));
