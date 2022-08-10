aa35e57e603fd6efa20995ef06900bc0a70674f4
Arif <marifullaah@gmail.com>
2022-08-10 15:40:31 +0600

    find positive

--- bestFriend.js
+++ bestFriend.js
@@ -1,8 +1,8 @@
-function bestFriend(friends) {
+const bestFriend = function (friends) {
     let largestName = friends[0];
     for (let x of friends) {
         if (x.length > largestName.length) largestName = x;
     }
     return largestName;
-}
+};
 console.log("the largest name is - " + bestFriend(["arif", "affif", "acib", "atik", "arifullah"]));
