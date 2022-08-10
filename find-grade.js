915379e99f39d0907c2901b63744abaa02730b81
Arif <marifullaah@gmail.com>
2022-08-10 16:53:52 +0600

    find grade of marks

--- find-grade.js
+++ find-grade.js
@@ -1,1 +1,28 @@
 
+function gradeCheck(marks) {
+    if (marks > 100 || marks < 0) {
+        return 'Invalid Marks';
+    }
+    else if (marks >= 80 && marks <= 100) {
+        return 'Your Grade is: A+'
+    }
+    else if (marks >= 70) {
+        return 'Your Grade is: A'
+    }
+    else if (marks >= 60) {
+        return 'Your Grade is: A-'
+    }
+    else if (marks >= 50) {
+        return 'Your Grade is: B'
+    }
+    else if (marks >= 40) {
+        return 'Your Grade is: C'
+    }
+    else if (marks >= 33) {
+        return 'Your Grade is: D'
+    }
+    else if (marks >= 0) {
+        return 'Your Grade is: F'
+    }
+}
+console.log(gradeCheck(98));
